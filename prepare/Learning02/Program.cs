dd65f56452e1166e2e3d4b6290b81e5b5ea0366e
ggosdinski <gianfranco.gosdinski@icloud.com>
2023-04-25 21:54:00 -0500

    Resume activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+       /*  Console.WriteLine($"{ job1._company}"); */
+        //job1.Display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        /* Console.WriteLine($"{ job2._company}"); */
+        //job2.Display();
+
+        Resume Resume = new Resume();
+        Resume._name = "Allison Rose";
+
+        Resume._jobs.Add(job1);
+        Resume._jobs.Add(job2);
+
+        Resume.Display();
     }
-}
+
+}   
